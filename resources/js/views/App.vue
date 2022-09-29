e592bdff729f5ec91a6530bf2dec324a3662c9cc
Antonius131 <antomendetta@gmail.com>
2022-09-29 15:05:47 +0200

    importing component in App

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,14 +1,18 @@
 <template>
    <section>
+      <MainHeader />
       <MainContent />
    </section>
 </template>
 
 <script>
    import MainContent from '../components/MainContent.vue';
+   import MainHeader from '../components/MainHeader.vue';
+
    export default {
       components: {
-         MainContent
+         MainContent,
+         MainHeader
       }
    }
 </script>
