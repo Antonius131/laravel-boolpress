78cc82f047b8b8e46ec33b139f84f74ec4df41dd
Antonius131 <antomendetta@gmail.com>
2022-09-29 15:10:34 +0200

    creating new TagList component

---
+++ resources/js/components/TagList.vue
@@ -0,0 +1,15 @@
+<template>
+   <section>
+      <ul class="list-group">
+         <li class="list-group-item">Tag</li>
+      </ul>
+   </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
